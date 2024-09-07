38d45ba12c5d4ea0d01af0d5f661700a27f3d41f
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-09-07 14:25:18 -0230

    new db connect

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import usersRouter from './routers/users';
 
 const app = new Hono();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.route('/users', usersRouter);
 
