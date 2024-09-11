c49e398e57a9c6cb726802e432fb39c3530b0137
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-09-10 22:17:32 -0230

    rds db

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,17 +1,45 @@
 import postgres from 'postgres';
+import 'dotenv/config';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-// only use dotenv in local dev
-if (process.env.NODE_ENV !== 'production') {
-  await import('dotenv/config');
+// Function to fetch certificates from S3
+async function getCerts() {
+  const s3 = new S3Client({ region: process.env.AWS_REGION });
+
+  try {
+    const data = await s3.send(
+      new GetObjectCommand({
+        Bucket: 'gameontap-server-cert',
+        Key: 'certkey.pem',
+      })
+    );
+
+    const chunks = [];
+    //@ts-ignore
+    for await (const chunk of data.Body) {
+      chunks.push(chunk);
+    }
+
+    return Buffer.concat(chunks).toString();
+  } catch (error) {
+    console.error('Error retrieving the certificate from S3:', error);
+    throw error;
+  }
 }
 
-// Function to create a PostgreSQL connection using the Railway-provided DATABASE_URL
+// Function to create a connection to PostgreSQL using certs
 async function createSqlConnection() {
   try {
-    //@ts-ignore
-    return postgres(process.env.DATABASE_CONNECT, {
+    const certs = await getCerts();
+
+    return postgres({
+      host: process.env.BEER_SCOUT_DB_HOST, // DB host for beer scout
+      port: Number(process.env.BEER_SCOUT_DB_PORT), // DB port for beer scout
+      database: process.env.BEER_SCOUT_DB_NAME, // DB name for beer scout
+      user: process.env.BEER_SCOUT_DB_USER, // DB user for beer scout
+      password: process.env.BEER_SCOUT_DB_PASSWORD, // DB password for beer scout
       ssl: {
-        rejectUnauthorized: false, // Disable strict SSL
+        ca: certs,
       },
     });
   } catch (error) {
@@ -20,7 +48,7 @@ async function createSqlConnection() {
   }
 }
 
-let sqlPromise: Promise<postgres.Sql<any>>;
+let sqlPromise: Promise<postgres.Sql>;
 
 export async function getSql() {
   if (!sqlPromise) {
