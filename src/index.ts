1329483e4471d990a0ae5d69520f47ca3d2c7166
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-10-15 18:24:40 -0230

    cors for custom domain

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-import barPricesRouter from './routers/barPrices';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import barPricesRouter from "./routers/barPrices";
 
 const app = new Hono();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: ["https://beerscout.ca/", "http://localhost:5173"] }));
 
-app.route('/barPrices', barPricesRouter);
+app.route("/barPrices", barPricesRouter);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
 });
 
 const port = 8787;
